025a68a6cfe6077072b455aeeb62f15d51504f8e
oqa5n5tangy <oqa5n5tangy@jichen.live>
2025-03-12 17:58:57 +0800

    first commit

---
+++ src/utils/pyselenium.py
@@ -0,0 +1,14 @@
+from selenium.webdriver import ChromeService
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver import FirefoxService
+from selenium.webdriver import FirefoxOptions
+
+class PySelenium :
+    __driver=None
+    __
+    def __new__(cls):
+        
+        pass
+
+    def __init__(self):
+        pass
